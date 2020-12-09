7f7d715f229ec2129966abf72833adb46f49ee85
catskillet.rsilva <catskillet.rsilva@hubfintech.com.br>
2020-12-09 19:24:11 -0300

    feat: add report view and urls

--- reports/views.py
+++ reports/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Report
+from .serializers import ReportSerializer
+
+
+class ReportViewSet(ModelViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    http_method_names = [
+        "get",
+    ]
